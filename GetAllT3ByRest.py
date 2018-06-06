c9255c413001ed29713e625c169a6a0ba9268b19
Adam Podsiadlo <13600045+AdamPodsiadlo@users.noreply.github.com>
2018-06-06 21:29:44 +0200

    Regex matching works

--- GetAllT3ByRest.py
+++ GetAllT3ByRest.py
@@ -1,6 +1,23 @@
 # python -m pip install request 
 # cmd to get request lib (module)
+# python -m pip install regex
+# cmd to get request lib (module)
 import requests
+import re
 url = 'https://www.olx.pl/motoryzacja/samochody/q-transporter-t3/'
 response = requests.get(url)
-print(response.text)
+#print(response.text)
+
+#matchLinkRegex = '<a class="thumb vtop inlblk rel tdnone linkWithHash scale4 detailsLinkPromoted "[\S\s]*href="([\S\s]*)" title="" >'
+matchLinkRegex = 'href="([\S\s]*?)"'
+matches = re.findall(matchLinkRegex, response.text)
+print('Matches:')
+print(matches)
+i = 0
+for match in matches:
+	print(i)
+	print(match)
+	i = i + 1
+
+
+
