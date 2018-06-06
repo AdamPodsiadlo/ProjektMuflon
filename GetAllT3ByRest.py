8a020d81782c3f4c0c0f9b5e3a1326410fe07108
Adam Podsiadlo <13600045+AdamPodsiadlo@users.noreply.github.com>
2018-06-06 23:50:39 +0200

    Added chart creation and showing.

--- GetAllT3ByRest.py
+++ GetAllT3ByRest.py
@@ -1,14 +1,20 @@
 # py -m pip install request 
 # cmd to get request lib (module)
 # py -m pip install regex
-# cmd to get request lib (module)
+# cmd to get regex lib (module)
+# py -m pip install pygal
+# cmd to get charts lib (module)
+# py -m pip install cairosvg
+# cmd to get lib which makes png images (for charts)
 import requests
 import re
+import pygal
+import os
 
 
 class RokPrzebieg:
-	Rok = ""
-	Przebieg = ""
+	Rok = 0
+	Przebieg = 0
 
 url = 'https://www.olx.pl/motoryzacja/samochody/q-transporter-t3/'
 response = requests.get(url)
@@ -39,13 +45,22 @@ for match in matches:
 	print('Przebieg: ',przebieg)
 	
 	rp = RokPrzebieg()
-	rp.Rok = rokProdukcji
-	rp.Przebieg = przebieg
-	
+	rp.Rok = int(rokProdukcji.strip())
+	rp.Przebieg = int(przebieg.strip().replace(' ',''))
+	if(rp.Przebieg > 1000000):
+		print("Przebieg nierealny. Oferta nie zostanie uwzgledniona w wykresie")
+		continue
+		
 	dane.append(rp)
 	i = i + 1
 	
-	
+graph = pygal.XY(stroke=False);
+graph.Title = 'Rok a przebieg'
 
+graph.add('', [(d.Rok, d.Przebieg) for d in dane])
+
+fileName = 'wykres.svg'
+graph.render_to_file(fileName)
+os.system(fileName);
 
 
