4197c72323c2fd89b52c83910d8c8a8ebe73563c
Adam Podsiadlo <13600045+AdamPodsiadlo@users.noreply.github.com>
2018-06-06 22:25:34 +0200

    improved link regexp
    added regex and request for rok produkcji and przebieg

--- GetAllT3ByRest.py
+++ GetAllT3ByRest.py
@@ -9,7 +9,9 @@ response = requests.get(url)
 #print(response.text)
 
 #matchLinkRegex = '<a class="thumb vtop inlblk rel tdnone linkWithHash scale4 detailsLinkPromoted "[\S\s]*href="([\S\s]*)" title="" >'
-matchLinkRegex = 'summary[\S\s]*?href="([\S\s]*?)"'
+matchLinkRegex = 'href="(https:\/\/www.olx.pl\/oferta.*?\.html#\w*?)"'
+rokProdukcjiRegex = 'Rok produkcji[\s\S]*?<strong>[\s\S]*?(\d\d\d\d)'
+przebiegRegex = 'Przebieg[\s\S]*?<strong>[\s\S]*?([\d\s]+)'
 matches = re.findall(matchLinkRegex, response.text)
 print('Matches:')
 print(matches)
@@ -17,6 +19,13 @@ i = 0
 for match in matches:
 	print(i)
 	print(match)
+	responseOffer = requests.get(match)
+	#print(responseOffer.text)
+	przebieg = re.findall(przebiegRegex, responseOffer.text)
+	rokProdukcji = re.findall(rokProdukcjiRegex, responseOffer.text)
+	rokProdukcji = re.findall(rokProdukcjiRegex, responseOffer.text)
+	print(rokProdukcji)
+	print(przebieg)
 	i = i + 1
 
 
